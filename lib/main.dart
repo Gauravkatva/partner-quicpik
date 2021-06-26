72ceb0f4f3b8108856b18682316402934441dd42
Tarang-1112 <70871372+Tarang-1112@users.noreply.github.com>
2021-06-26 16:43:13 +0530

    welcome added

--- lib/main.dart
+++ lib/main.dart
@@ -97,7 +97,7 @@ class _MyHomePageState extends State<MyHomePage> {
               'You have pushed the button this many times:',
             ),
             Text(
-              '$_counter',
+              'Welcome $_counter',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
